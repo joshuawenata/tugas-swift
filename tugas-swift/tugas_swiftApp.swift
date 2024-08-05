039b5cd21d99397b9ae40989d58744c39077f4c5
joshuawenata <joshuawenata@gmail.com>
2024-08-05 19:43:59 +0700

    feat: init project

---
+++ tugas-swift/tugas_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  tugas_swiftApp.swift
+//  tugas-swift
+//
+//  Created by Joshua Wenata Sunarto on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct tugas_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tugas-swiftUITests/tugas_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tugas_swiftUITestsLaunchTests.swift
+//  tugas-swiftUITests
+//
+//  Created by Joshua Wenata Sunarto on 05/08/24.
+//
+
+import XCTest
+
+final class tugas_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
